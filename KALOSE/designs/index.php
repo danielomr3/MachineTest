c85e336c5811a976b4876b004174d69812bb6ef9
danielomr3 <71662805+danielomr3@users.noreply.github.com>
2021-07-03 15:53:11 +0300

    initial commit

---
+++ KALOSE/designs/index.php
@@ -0,0 +1,290 @@
+<!doctype html>
+<html>
+<head>
+<meta id="viewport" name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width" />
+<title>Dashboard</title>
+<link href="../css/bootstrap.min.css" rel="stylesheet" />
+<link href="../css/slick.css" rel="stylesheet" />
+<link href="../css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+<div class="page-wrapper"> 
+  <!--HEADER SECTION-->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-7 bg-green">
+        <div class="logo-text">KALOSE</div>
+      </div>
+      <div class="col-5"> 
+        <!--NAVIGATION SECTION-->
+        <div class="navigation-section">
+          <ul>
+            <span class="menu-close" title="Close"><img src="../images/menu-close.jpg" /></span>
+            <li><a href="" title="How it works">How it works</a></li>
+            <li><a href="" title="Products">Products</a></li>
+            <li><a href="" title="Contact Us">Contact Us</a></li>
+          </ul>
+          <div class="menu-icon"><img src="../images/menu-icon.png" alt="user image"/></div>
+          <div class="user-profile"><img src="../images/user-image.jpg" alt="user image"/></div>
+          <div class="cart-info"><img src="../images/cart-image.jpg" alt="Cart info" /></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--HEADER SECTION-->
+  <div class="container-fluid top-info">
+    <div class="row">
+      <div class="col-md-7 bg-green">
+        <div class="box-info">
+          <div class="info-txt1">BEAUTY</div>
+          <div class="info-txt2">We'll take<br/>
+            It from here</div>
+          <div class="info-txt3">Skin care is the range of practises that<br/>
+            support skin integrity</div>
+          <div class="info-link"> <a href="" title="">Check Product</a> <a href="" title="">See our Video</a> </div>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="image-position-type1"><img class="img-fluid" src="../images/banner-image1.jpg" alt="banner image"/></div>
+      </div>
+    </div>
+  </div>
+  <!--PRODUCT LISTING-->
+  <div class="product-listing">
+    <div class="container">
+      <div class="heading-type1 text-center">Skin Care Products <br style="display:none;"/>
+        <a href="" title="Shop Now" class="link-type1">Shop Now</a></div>
+      <div class="row">
+        <div class="col-6 col-md-3 text-center">
+          <div class="product-box">
+            <div class="prd-item"><img src="../images/product-1.jpg" alt /></div>
+            <div class="prd-name">Skin &amp; Coat</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 text-center">
+          <div class="product-box">
+            <div class="prd-item"><img src="../images/product-2.jpg" alt /></div>
+            <div class="prd-name">Skin &amp; Coat</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 text-center">
+          <div class="product-box">
+            <div class="prd-item"><img src="../images/product-3.jpg" alt /></div>
+            <div class="prd-name">Skin &amp; Coat</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 text-center">
+          <div class="product-box">
+            <div class="prd-item"><img src="../images/product-4.jpg" alt /></div>
+            <div class="prd-name">Skin &amp; Coat</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--TESTIMONIAL SECTION-->
+  <div class="testimonial-box">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="testimonial-txt-box1">
+            <div class="heading-type1">What Our Clients Say About Us </div>
+            <div class="testimonial-subtxt"><a href="#" title="Watch Video">Watch Video</a></div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="testimonial-message-box clearfix">
+            <div class="testimonial-msg">It is a long established fact that a reader will be distracted by the readable content.</div>
+            <div class="testimonial-autor">
+              <div class="testimonial-autor-txt1">Aleena James</div>
+              <div class="testimonial-autor-txt2">Modeling Artist</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--DRUG SLIDER SECTION-->
+  <div class="medicine-section">
+    <div class="container">
+      <div class="heading-type1 text-center">Vitamins tablets <br style="display:none;"/><a href="" title="Shop Now" class="link-type2">Shop Now</a></div>
+    </div>
+    <div class="container-fluid">
+      <div class="medicine-slider">
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image1.jpg" alt=""></div>
+            <div class="prd-name">Naturelo</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image2.jpg" alt=""></div>
+            <div class="prd-name">Naturelo</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image3.jpg" alt=""></div>
+            <div class="prd-name">Constipation</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image4.jpg" alt=""></div>
+            <div class="prd-name">Cymbalta</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image1.jpg" alt=""></div>
+            <div class="prd-name">Naturelo</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image2.jpg" alt=""></div>
+            <div class="prd-name">Naturelo</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image3.jpg" alt=""></div>
+            <div class="prd-name">Constipation</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+        <div>
+          <div class="product-box text-center">
+            <div class="prd-item"><img src="../images/medicine-image4.jpg" alt=""></div>
+            <div class="prd-name">Cymbalta</div>
+            <div class="prd-price">$20 - $25</div>
+            <div class="prd-content">Premium support for healthy skin and shiny coat</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--OUR BENEFITS SECTION-->
+  <div class="our-benefits-box">
+    <div class="container">
+      <div class="heading-type1 text-center">Our Benefits</div>
+      <div class="our-benefits-info font-0 text-center">
+        <div class="our-benefits-info-item text-center">
+          <div class="benefit-img"><img src="../images/benefits-image.jpg" alt="Benefit image"/></div>
+          <div class="benefit-txt">High speed delivery, anytime and from anywhere</div>
+        </div>
+        <div class="our-benefits-info-item text-center">
+          <div class="benefit-img"><img src="../images/benefits-image2.jpg" alt="Benefit image"/></div>
+          <div class="benefit-txt">More than 1000 stores worldwide.</div>
+        </div>
+        <div class="our-benefits-info-item text-center">
+          <div class="benefit-img"><img src="../images/benefits-image3.jpg" alt="Benefit image"/></div>
+          <div class="benefit-txt">We have 24 hrs customer support team</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Footer SECTION-->
+  <div class="bg-green footer-section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="footer-txt-box1">
+            <div class="heading-type1">It’s Glow <br/>
+              Time.</div>
+            <div class="footer-subtxt">© 2020 KALOSE LLC. Legal.</div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-6">
+              <div class="footer-menu-hd">About Us</div>
+              <ul class="footer-menu">
+                <li><a>Co-living</a></li>
+                <li><a>FAQs</a></li>
+                <li><a>Careers</a></li>
+                <li><a>The Journal</a></li>
+                <li><a>Serviced Living</a></li>
+                <li><a>Venue hire</a></li>
+                <li><a>Co-working</a></li>
+              </ul>
+            </div>
+            <div class="col-6">
+              <div class="footer-menu-hd">Get  in touch</div>
+              <ul class="footer-menu">
+                <li>KALOSE Inc,<br/>
+                  Century Park East Suite 803,<br/>
+                  Los Angeles,<br/>
+                  <a>+44 (0) 207 543 5478</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-subtxt coppy-right">© 2020 KALOSE LLC. Legal.</div>
+  </div>
+</div>
+
+<script type="text/javascript" src="../js/jquery-3.5.1.min.js"></script> 
+<script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script> 
+<script type="text/javascript" src="../js/slick.min.js"></script> 
+<script>
+$(document).ready(function() {
+	$('.medicine-slider').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 2,
+	   centerPadding: '100px',
+	   infinite: true,
+	    responsive: [
+    {
+	  breakpoint: 979,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+	   breakpoint: 599,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    
+	]
+	});
+	$(".menu-icon").click(function() {
+        $("body").toggleClass("menu-show");
+    });
+	$(".menu-close").click(function() {
+        $("body").toggleClass("menu-show");
+    }); 
+});
+</script>
+</body>
+</html>
